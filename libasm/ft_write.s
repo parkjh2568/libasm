041ca3b9f3093fd1497c5cf59b29e2aa32fbc540
Junhyun Park <junhypar@student.42seoul.kr>
2021-02-24 14:30:39 +0900

    yea finish him

--- libasm/ft_write.s
+++ libasm/ft_write.s
@@ -1,7 +1,17 @@
 section .text
 	global _ft_write
+	extern ___error
 
 _ft_write:
 			mov		rax, 0x2000004 ;write함수 등판
 			syscall
+			jc		error
 			ret
+
+error:
+			push	rax			;에러상황이므로 에러코드가 rax에 기입(스텍에 넣어두기)
+			call	___error
+			pop		r8			;에러코드받아옴
+			mov		[rax], r8	;[rax]에 에러코드저장이라는데 뭔소리인지는 모르겠음
+			mov		rax, -1
+			ret	
--- libasm/main.c
+++ libasm/main.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
 
-int ft_strcmp(char *a, char *b);
+int ft_strcmp(const char *a, const char *b);
+size_t ft_strlen(const char *a);
+char *ft_strcpy(char *a, const char *b);
+size_t	ft_write(int fildes, const void *buf, size_t nbyte);
+size_t	ft_read(int fildes, const void *buf, size_t nbyte);
+char *ft_strdup(const char *s1);
+
 int main()
 {
-	printf("%d",ft_strcmp("\xff\xff","\xff"));
+	char *a;
+	char *b = "copy done";
+	char c[100];
+	char *d = "strdup done";
+	char f[100];
+	printf("\"0123456789\" strlen = %zu\n",ft_strlen("0123456789"));
+	printf("\"0123456\", \"0123456\" strcmp = %d\n",ft_strcmp("0123456","0123456"));
+	printf("\"0123456\", \"0123\" strcmp = %d\n",ft_strcmp("0123456", "0123"));
+	printf("\"0123456\", \"0123456789\" strcmp = %d\n",ft_strcmp("0123456", "0123456789"));
+	ft_strcpy(c,b);
+	printf("strcpy = %s\n",c);	
+	a = ft_strdup(d);
+	printf("strdup = %s\n",a);
+	free(a);
+	ft_write(1,"ft_write_done\n",14);
+	printf("ft_read start--------> type any word(less 10)\n");
+	ft_read(0, f, 10);
+	printf("%s",f);
 }
